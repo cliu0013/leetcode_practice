b0889bf981d6e7aefdc1b97926c5a8787aa4a8cd
cliu0013 <lcj420106@yahoo.com>
2020-09-25 20:49:13 -0400

    two sum sorted

---
+++ TwoSumII.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        # Two Pointers
+        
+        pointer1 = 0
+        pointer2 = len(numbers) -1
+        
+        while pointer1 != pointer2:
+            if numbers[pointer1] + numbers[pointer2] == target:
+                return [pointer1+1, pointer2+1]
+            elif numbers[pointer1] + numbers[pointer2] > target:
+                pointer2 -= 1
+            elif numbers[pointer1] + numbers[pointer2] < target:
+                pointer1 += 1
+            
