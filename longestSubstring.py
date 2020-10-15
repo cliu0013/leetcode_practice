4081cdf99a39ce2351340c80afe86cd5900e8499
cliu0013 <lcj420106@yahoo.com>
2020-10-15 00:01:42 -0400

    DP longest substring

--- longestSubstring.py
+++ longestSubstring.py
@@ -1,3 +1,25 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        
+    # very slow DP, but very little space
+        if len(s) == 0:
+            return 0
+        all_length = set()
+        all_length.add(1)
+        st = s
+        while st != "" and len(st) != 1:
+            substr = {}
+            length = 0
+            for i in range(0, len(st)):
+                if st[i] not in substr.keys():
+                    substr[st[i]] = i
+                    length += 1
+                else:
+                    pos = substr[st[i]]
+                    st = st[pos+1:]
+                    break
+                if i == len(st) -1:
+                    st = ""
+                
+            all_length.add(length)
+        return max(all_length)
+            
