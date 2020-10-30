48566dfad3040d2e7cc946a9088edc07c2e75738
cliu0013 <lcj420106@yahoo.com>
2020-10-30 17:15:50 -0400

    permutation

--- permutation.py
+++ permutation.py
@@ -4,20 +4,34 @@ class Solution:
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        def backtrack(first = 0):
-            # if all integers are used up
-            if first == n:  
+        
+        def bt(first = 0):
+            if first == n - 1:
                 output.append(nums[:])
             for i in range(first, n):
-                # place i-th integer first 
-                # in the current permutation
                 nums[first], nums[i] = nums[i], nums[first]
-                # use next integers to complete the permutations
-                backtrack(first + 1)
-                # backtrack
-                nums[first], nums[i] = nums[i], nums[first]
-        
+                bt(first + 1)
+                nums[first], nums[i] = nums[i], nums[first] # wrap back to the original nums before the loop
         n = len(nums)
         output = []
-        backtrack()
+        bt()
         return output
+                
+            
+#         def backtrack(first = 0):
+#             # if all integers are used up
+#             if first == n:  
+#                 output.append(nums[:])
+#             for i in range(first, n):
+#                 # place i-th integer first 
+#                 # in the current permutation
+#                 nums[first], nums[i] = nums[i], nums[first]
+#                 # use next integers to complete the permutations
+#                 backtrack(first + 1)
+#                 # backtrack
+#                 nums[first], nums[i] = nums[i], nums[first]
+        
+#         n = len(nums)
+#         output = []
+#         backtrack()
+        # return output
