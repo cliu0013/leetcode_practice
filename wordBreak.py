2d64a61f8772181771cca7fa48d803db37e1eb79
cliu0013 <lcj420106@yahoo.com>
2020-10-31 16:45:03 -0400

    wordBreak optimal

--- wordBreak.py
+++ wordBreak.py
@@ -5,9 +5,27 @@ class Solution(object):
         :type wordDict: List[str]
         :rtype: bool
         """
-        # O(n^2), O(n)
-        dp = [True]*(len(s) + 1)
+        # # O(n^3) DP
+        # dp = [True]*(len(s) + 1)
+        # words = set(wordDict)
+        # for i in range(1, len(s)+1):
+        #     dp[i] = any(dp[j] and s[j:i] in words for j in range(i))
+        # return dp[-1]
+    
+        # O(n^2) DFS
+        memo = {}
         words = set(wordDict)
-        for i in range(1, len(s)+1):
-            dp[i] = any(dp[j] and s[j:i] in words for j in range(i))
-        return dp[-1]
+        def dfs(start):
+            if start == len(s):
+                return True
+            if start in memo:
+                return memo[start]
+            for i in range(start+1, len(s)+1):
+                if s[start:i] in words and dfs(i):
+                    memo[start] = True
+                    return True
+            memo[start] = False
+            return False
+    
+        return dfs(0)
+        
