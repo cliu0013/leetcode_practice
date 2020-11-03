fe0e9816d29a02294a59dd828834d6243e2a785a
cliu0013 <lcj420106@yahoo.com>
2020-11-03 18:27:05 -0500

    meeting room II

---
+++ minMeetingRoomsII.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minMeetingRooms(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+        # O(nlonn), O(n)
+        if len(intervals) == 0: return 0
+        free_rooms = []
+        intervals.sort(key = lambda x: x[0])
+        
+        heapq.heappush(free_rooms, intervals[0][1])
+        
+        for i in intervals[1:]:
+            if i[0] >= free_rooms[0]:
+                heapq.heappop(free_rooms)
+            heapq.heappush(free_rooms, i[1])
+        
+        return len(free_rooms)
