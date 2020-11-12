297020cccf8f5b0d67d761667d39ea4a194bbbe6
Chengji Liu <lcj420106@yahoo.com>
2020-11-12 11:59:49 -0500

    dp maximum product

--- maximumProductSubarray.py
+++ maximumProductSubarray.py
@@ -4,7 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # traditional DP
+        # if not nums:
+        #     return 0
         
+        # cur = nums[0]
+        # max_so_far = [cur]
+        # min_so_far = [cur]
+      
+        
+        # for i, ele in enumerate(nums[1:]):
+        #     last_max = max_so_far[i]
+        #     last_min = min_so_far[i]
+        #     max_so_far.append(max(last_max * ele, last_min * ele, ele))
+        #     min_so_far.append(min(last_max * ele, last_min * ele, ele))
+        
+        # return max(max_so_far)        
         def product(arr):
             if len(arr) == 0: return -float('inf')
             if len(arr) == 1: return arr[0]
